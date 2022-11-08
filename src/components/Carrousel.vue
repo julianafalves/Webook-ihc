27374903c0297be583951b6edfee2750e0be60d7
AnaClaraAmorim <anaclaramorimandrade@gmail.com>
2022-11-08 00:11:17 -0300

    change images carrousel

--- src/components/Carrousel.vue
+++ src/components/Carrousel.vue
@@ -1,23 +1,32 @@
 <template>
-  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="../components/banner/banner1.webp" alt="First slide">
+      <img class="d-block w-100" src="../components/banner/banner1.png" alt="Primeiro Slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="../components/banner/banner2.jpg" alt="Second slide">
+      <img class="d-block w-100" src="../components/banner/banner2.jpg" alt="Segundo Slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="../components/banner/banner3.webp" alt="Third slide">
+      <img class="d-block w-100" src="../components/banner/banner3.webp" alt="Terceiro Slide">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
+    <span class="sr-only">Anterior</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
+    <span class="sr-only">Próximo</span>
   </a>
 </div>
 </template>
+
+
+
+
